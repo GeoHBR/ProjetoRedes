df1570432e1597f8d8633e75ed0531c928640925
Gilmar <geoh222@unesc.net>
2023-11-14 17:07:22 -0300

    Parou de Funcionar, tava funcionando client1 add palavra e client2 acertar ela mas parou, só deus sabe pq, depois eu tento descobrir

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,55 +1,72 @@
 import java.io.*;
 import java.net.*;
 import javax.swing.JOptionPane;
+import java.util.*;
 
 public class Client {
     public static void main(String[] args) {
         try {
+            String IpServer = JOptionPane.showInputDialog("Digite o IP do servidor padrão local: localhost");
+
+            // Conectar ao servidor na porta 12345
+            Socket socket = new Socket(IpServer, 12345);
+
+            // Obter streams de entrada e saída do cliente
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+
+            // Solicitar ao usuário que digite seu nome
+            String nomeCliente = JOptionPane.showInputDialog("Digite seu nome:");
+
+            // Enviar nome ao servidor
+            out.println(nomeCliente);
+
+            // Loop principal do cliente
             while (true) {
+                String opcao = JOptionPane.showInputDialog("Selecione uma Opção:\n1-Escolher Palavra\n2-Adivinhar Palavra\n3-Sair");
 
-                String IpServer = JOptionPane.showInputDialog("Digite o IP do servidor, padrão local: localhost:");
+                if (opcao.equals("1")) {
+                    // Solicitar ao usuário que escolha um tema
+                    String[] temas = {"Animais", "Cores", "Frutas"}; // Adicione mais temas conforme necessário
+                    String temaEscolhido = (String) JOptionPane.showInputDialog(null, "Escolha um tema:", "Escolha de Tema", JOptionPane.PLAIN_MESSAGE, null, temas, temas[0]);
 
+                    // Solicitar a palavra aleatória do tema escolhido
+                    out.println("TEMADOJOGO:" + temaEscolhido);
 
-                Socket socket = new Socket(IpServer, 12345);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-                PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+                    // Receber resposta do servidor (palavra aleatória)
+                    String palavraAleatoria = in.readLine();
+                    JOptionPane.showMessageDialog(null, "Palavra do tema '" + temaEscolhido + "': " + palavraAleatoria);
 
-                String nomeCliente = JOptionPane.showInputDialog("Digite seu nome:");
-                out.println(nomeCliente);
+                    // Continue com o jogo normalmente (envie uma mensagem ao servidor)
+                    String message = JOptionPane.showInputDialog("Digite uma mensagem para o servidor:");
 
-                // Loop pra enviar palavras
-                while (true) {
-                    String opcao = JOptionPane.showInputDialog("Escolha uma opção:\n1-Escolher Palavra\n2-Adivinhar Palavra\n3-Sair");
-
-                    if (opcao.equals("1")) {
-                        String[] temas = {"Animais", "Cores", "Frutas"};
-                        String temaEscolhido = (String) JOptionPane.showInputDialog(null, "Escolha um tema:", "Escolha de Tema", JOptionPane.PLAIN_MESSAGE, null, temas, temas[0]);
-
-                        out.println("TEMADOJOGO:" + temaEscolhido);
-
-
-                        String palavraAleatoria = in.readLine();
-                        JOptionPane.showMessageDialog(null, "Palavra do tema '" + temaEscolhido + "': " + palavraAleatoria);
-
-
-                        String message = JOptionPane.showInputDialog("Digite uma mensagem para o servidor:");
-
-                        // Verifica se o user escreveu algo
-                        if (message != null && !message.trim().isEmpty()) {
-                            out.println(message);
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Você não digitou uma palavra. Tente novamente.");
-                            continue; // Volta ao início do loop
-                        }
+                    // Verifique se o usuário inseriu uma palavra antes de enviar ao servidor
+                    if (message != null && !message.trim().isEmpty()) {
+                        out.println("ESCREVERPALAVRA:" + message);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Você não digitou uma palavra. Tente novamente.");
+                        //continue; // Volte ao início do loop
                     }
+                } else if (opcao.equals("2")) {
+                    // Solicitar ao usuário que adivinhe a palavra
+                    String tentativa = JOptionPane.showInputDialog("Digite sua tentativa:");
 
-                    // Opção de encerrar conexão
-                    if (opcao.equals("3")) {
+                    // Enviar tentativa ao servidor
+                    out.println("ADIVINHARPALAVRA:" + tentativa);
+
+                    // Receber resposta do servidor e exibir usando JOptionPane
+                    String serverResponse = in.readLine();
+                    JOptionPane.showMessageDialog(null, "Resposta do Servidor:\n" + serverResponse);
+                } else if (opcao.equals("3")) {
+                    // Solicitar ao usuário se deseja realmente sair
+                    int confirmacao = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja sair?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                    if (confirmacao == JOptionPane.YES_OPTION) {
                         JOptionPane.showMessageDialog(null, "Conexão Encerrada");
+                        // Fechar conexões e sair
                         in.close();
                         out.close();
                         socket.close();
-                        return;
+                        System.exit(0);
                     }
                 }
             }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,9 +5,10 @@ import java.util.*;
 public class Server {
     private static Map<Socket, String> clientMap = new HashMap<>();
     private static Map<String, List<String>> temasPalavras = new HashMap<>();
+    private static Map<String, String> palavraOriginalMap = new HashMap<>();
 
     public static void main(String[] args) {
-        // Lista de palavras dos temas
+        // Adicione palavras aos temas conforme necessário
         List<String> palavrasAnimais = Arrays.asList("Elefante", "Girafa", "Leão", "Macaco");
         List<String> palavrasCores = Arrays.asList("Vermelho", "Azul", "Verde", "Amarelo");
         List<String> palavrasFrutas = Arrays.asList("Maçã", "Banana", "Uva", "Morango");
@@ -17,15 +18,17 @@ public class Server {
         temasPalavras.put("Frutas", palavrasFrutas);
 
         try {
+            // Criação do servidor socket na porta 12345
             ServerSocket serverSocket = new ServerSocket(12345);
+
             System.out.println("Servidor esperando por conexões...");
 
             while (true) {
-                // Esperando um Client
+                // Aguardar a conexão de um cliente
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Cliente conectado: " + clientSocket.getInetAddress().getHostAddress());
 
-                // Cria uma nova thread para o cliente
+                // Criar uma nova thread para o cliente
                 Thread clientThread = new Thread(() -> handleClient(clientSocket));
                 clientThread.start();
             }
@@ -40,32 +43,44 @@ public class Server {
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
             PrintWriter out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"), true);
 
-            //Nome do cliente
+            // Receber o nome do cliente
             String nomeCliente = in.readLine();
             System.out.println("Cliente '" + nomeCliente + "' identificado.");
 
-            // Associa nome do cleint com o socket
+            // Associar o nome do cliente ao socket
             clientMap.put(clientSocket, nomeCliente);
 
-            // Loop das msgs
+            // Loop para troca de mensagens
             while (true) {
                 String clientMessage = in.readLine();
                 System.out.println(nomeCliente + ": " + clientMessage);
 
                 if (clientMessage.startsWith("TEMADOJOGO:")) {
-
+                    // Extrair o tema escolhido
                     String temaEscolhido = clientMessage.substring("TEMADOJOGO:".length());
 
-                    // Pega a palavra
+                    // Obter uma palavra aleatória do tema escolhido
                     String palavraAleatoria = obterPalavraAleatoria(temaEscolhido);
 
-                    // Envia a palavra aleatoria pro client
+                    // Armazenar a palavra original no mapa para posterior verificação
+                    palavraOriginalMap.put(nomeCliente, palavraAleatoria);
+
+                    // Enviar a palavra aleatória ao cliente
                     out.println(palavraAleatoria);
-                } else {
-                    /* Se a mensagem não começar com "TEMADOJOGO:", considera como a mensagem escrita pelo cliente,
-                    botei isso pq primeiro chega pro server a resposta do tema, e ele tratava como se fosse a msg escrita pelo client*/
-                    System.out.println("Mensagem escrita por " + nomeCliente + ": " + clientMessage);
+                } else if (clientMessage.startsWith("ADIVINHARPALAVRA:")) {
+                    // Extrair a tentativa de adivinhar
+                    String tentativa = clientMessage.substring("ADIVINHARPALAVRA:".length());
+
+                    // Verificar se a tentativa é correta
+                    String palavraOriginal = palavraOriginalMap.get(nomeCliente);
+                    if (palavraOriginal != null && tentativa.equalsIgnoreCase(palavraOriginal)) {
+                        out.println("Parabéns! Você acertou a palavra.");
+                    } else {
+                        out.println("Palavra incorreta. Tente novamente.");
+                    }
                 }
+
+                // Verificar se o cliente quer encerrar a conexão
                 if (clientMessage.equalsIgnoreCase("exit")) {
                     break;
                 }
